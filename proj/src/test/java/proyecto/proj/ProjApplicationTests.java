81a6190160788ad16643591c2f88d01050b5f77a
AlexanderLira <liracaceres12345@gmail.com>
2026-03-01 19:59:27 +0100

    Agregar comentarios al test contextLoads en ProjApplicationTests

--- proj/src/test/java/proyecto/proj/ProjApplicationTests.java
+++ proj/src/test/java/proyecto/proj/ProjApplicationTests.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjApplicationTests {
+class ProjApplicationTests 
+{
 
+	// Prueba generada autoamaticamente por Springboot, se asegura que el contexto de la aplicacion se cargue
+	// Si no se lanzan excepciones, la prueba pasa indicando que la aplicacion esta bien configurada.
 	@Test
 	void contextLoads() {
 	}
